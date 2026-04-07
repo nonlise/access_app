56a7dd3db59baeeee300039db42b4e06f524b4b3
nonlise <sogoodlive0@gmail.com>
2026-04-08 01:06:50 +0300

    fix error sql connection

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from app.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={"check_same_thread": True} if "sqlite" in settings.DATABASE_URL else {}
+    connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {} #Первый запрос с ошибкой, попытка использовать старое соединение из другого потока, при "check_same_thread": True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
